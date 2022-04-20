bbab25c25f075cb15d8655264c366ea4c1f46df1
Ushiiin <yagyudon@hotmail.com>
2022-04-20 21:10:03 +0900

    追加したセルを操作可能

--- js/landmine.js
+++ js/landmine.js
@@ -1,4 +1,3 @@
-
 var H = 0;
 var W = 0;
 
@@ -13,7 +12,10 @@ $("#generator").on("click", function () {
         for (j = 0; j < W; j++){
             const idx = i * W + j
             // console.log(idx);
-            gyou += '<td id="' + idx + 'ban"></td>'
+            // gyou += '<td id="' + idx + 'ban"></td>'
+            gyou += `
+                <td id=${idx}ban></td>
+            `
         }
         gyou += "</tr>"
         console.log(gyou);
@@ -21,13 +23,56 @@ $("#generator").on("click", function () {
     }
 });
 
-for (i = 0; i < H; i++){
-    for (j = 0; j < W; j++) {
-        const idx = i * H + j;
-        const ind = '#' + idx + 'ban';
-        console.log(ind);
-        $("#0ban").on("click", function () {
-            alert("A");
-        })
+//ここより上は動作する。
+
+$("test").on("click", function() {
+    alert("X");
+})
+
+//ここのFor loopの部分が動作しない。
+
+
+// // var ban = document.getElementById('test');//テーブルを取得
+
+// //クリックした時のイベント
+// for (var x=0; x < 2;x++){
+//     for(var y = 0; y < 2; y++){
+//         var oseta = ban.rows[x].cells[y];
+//         console.log("X")
+//         console.log(oseta);
+//         oseta.onclick = function(){Oseta(this);}
+//     }
+// }
+// function Oseta(Cell){
+//     alert("A");
+//     // Cell.innerText ="押した";
+//     Cell.innerText = "1";
+// };
+
+var banmen = document.getElementById("field");//テーブルを取得
+
+$(document).on("click", "#0ban", function() {
+    alert("Z");
+    alert("ZZ")
+});
+
+const MAX = 15
+
+for (i = 0; i < MAX; i++){
+    for (j = 0; j < MAX; j++) {
+        const num = i*MAX + j;
+        let ind = "#" + num  + "ban"
+        // console.log(ind);
+        $(document).on("click", ind, function() {
+            // console.log("10");
+            // alert(this);
+            $(this).css('background-color', 'red');
+        });
     }
 }
+
+function Oseta(Cell){
+    alert("A");
+    // Cell.innerText ="押した";
+    Cell.innerText = "1";
+};
